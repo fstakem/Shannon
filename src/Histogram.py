9d9b77194154ae71f136626db1a9a7123a18366d
Fred Stakem <fstakem@gmail.com>
2014-08-11 22:35:23 -0400

    Adding virtualenv and first files.

---
+++ src/Histogram.py
@@ -0,0 +1,8 @@
+"""
+    File: Histogram.py
+    By: Fredrick Stakem
+    Date: 8.11.14
+
+    Purpose: To perform histogram on image data
+"""
+
---
+++ test/TestHistogram.py
@@ -0,0 +1,21 @@
+"""
+    File: TestHistogram.py
+    By: Fredrick Stakem
+    Date: 8.11.14
+
+    Purpose: To perform test on histogram of image data
+"""
+
+# Libraries
+import unittest
+
+# Main
+class TestHistogram(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
